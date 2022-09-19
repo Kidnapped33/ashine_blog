74e714f512e2031cc91a32da6f54d28506e090b5
ashine33 <ashine33@foxmail.com>
2022-09-19 10:32:08 +0800

    添加导航栏

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,16 @@
+export default {
+    title: 'VitePress',
+    description: 'Just playing around.',
+    themeConfig: {
+        siteTitle: '前端',
+        logo: '/study.svg',
+        // siteTitle: false, // 禁用标题
+      },
+      themeConfig:{
+        nav: [
+            { text: 'GitHub', link: 'https://github.com/Kidnapped33' },
+            { text: '语雀', link: 'https://www.yuque.com/ashine33' },
+            { text: '联系我', link: '/' },
+        ]
+      }
+}
