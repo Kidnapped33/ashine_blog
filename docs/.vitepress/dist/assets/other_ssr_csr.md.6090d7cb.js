6554d3b85ec1de449675a321d2fc8c753d1188a9
ashine33 <ashine33@foxmail.com>
2023-02-13 19:49:10 +0800

    build

---
+++ docs/.vitepress/dist/assets/other_ssr_csr.md.6090d7cb.js
@@ -0,0 +1,8 @@
+import{_ as e,c as r,o as s,a}from"./app.bee700b0.js";const u=JSON.parse('{"title":"SSR VS CSR","description":"","frontmatter":{"navbar":true},"headers":[{"level":3,"title":"SSR","slug":"ssr","link":"#ssr","children":[]},{"level":3,"title":"CSR","slug":"csr","link":"#csr","children":[]}],"relativePath":"other/ssr_csr.md"}'),t={name:"other/ssr_csr.md"},n=a(`<h1 id="ssr-vs-csr" tabindex="-1">SSR VS CSR <a class="header-anchor" href="#ssr-vs-csr" aria-hidden="true">#</a></h1><h3 id="ssr" tabindex="-1">SSR <a class="header-anchor" href="#ssr" aria-hidden="true">#</a></h3><pre><code>* \u5168\u79F0\uFF1AServer Side Render\uFF08\u670D\u52A1\u5668\u7AEF\u6E32\u67D3\uFF09
+    1\u3001\u9875\u9762\u4E0A\u7684\u5185\u5BB9\u662F\u901A\u8FC7\u670D\u52A1\u7AEF\u6E32\u67D3\u751F\u6210\u7684
+    2\u3001\u6D4F\u89C8\u5668\u76F4\u63A5\u663E\u793A\u670D\u52A1\u7AEF\u8FD4\u56DE\u7684html\u5C31\u53EF\u4EE5\u4E86
+</code></pre><h3 id="csr" tabindex="-1">CSR <a class="header-anchor" href="#csr" aria-hidden="true">#</a></h3><pre><code>* \u5168\u79F0\uFF1AClient Side Render\uFF08\u6D4F\u89C8\u5668\u7AEF\u6E32\u67D3\uFF09
+    1\u3001\u9875\u9762\u4E0A\u7684\u5185\u5BB9\u662F\u6211\u4EEC\u52A0\u8F7D\u7684js\u6587\u4EF6\u6E32\u67D3\u51FA\u6765\u7684
+    2\u3001js\u6587\u4EF6\u8FD0\u884C\u5728\u6D4F\u89C8\u5668\u4E0A\u9762
+    3\u3001\u670D\u52A1\u7AEF\u53EA\u8FD4\u56DE\u4E00\u4E2Ahtml\u6A21\u677F
+</code></pre>`,5),c=[n];function d(i,o,_,h,l,S){return s(),r("div",null,c)}const R=e(t,[["render",d]]);export{u as __pageData,R as default};
